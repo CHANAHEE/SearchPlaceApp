85a0e0aa7bc1c1df3ea6e83168fe974ba07713cd
CHANAHEE <tjdrjs0803@naver.com>
2023-04-03 10:34:49 +0900

    LoginActivity 완료

---
+++ app/src/main/java/com/example/tp_searchplaceapp/model/NidUserInfoResponse.kt
@@ -0,0 +1,7 @@
+package com.example.tp_searchplaceapp.model
+
+
+// 73_ Gson 으로 받아올 데이터를 데이터 클래스에 저장하기 위해...
+data class NidUserInfoResponse(var resultcode:String,var message:String,var response:NidUser)
+
+data class NidUser(var id: String, var email: String)
