f36f430def0aba087db8d5d70430b8a7cc13fc9d
CHANAHEE <tjdrjs0803@naver.com>
2023-04-03 11:00:14 +0900

    MainActivity toolbar 작업

--- app/src/main/java/com/example/tp_searchplaceapp/activities/MainActivity.kt
+++ app/src/main/java/com/example/tp_searchplaceapp/activities/MainActivity.kt
@@ -2,11 +2,35 @@ package com.example.tp_searchplaceapp.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
 import com.example.tp_searchplaceapp.R
+import com.example.tp_searchplaceapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.option,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId){
+            R.id.menu_aa->Toast.makeText(this, "aa", Toast.LENGTH_SHORT).show()
+            R.id.menu_bb->Toast.makeText(this, "bb", Toast.LENGTH_SHORT).show()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
